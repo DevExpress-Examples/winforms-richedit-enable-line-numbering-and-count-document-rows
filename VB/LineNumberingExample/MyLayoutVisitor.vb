44e6d2ef50569089d45a9780ea41dad952ce9799
DevExpressExampleBot <fakeEmail>
2020-04-17 17:04:04 +0300

    Source auto update [skip ci]

--- VB/LineNumberingExample/MyLayoutVisitor.vb
+++ VB/LineNumberingExample/MyLayoutVisitor.vb
@@ -3,31 +3,31 @@ Imports DevExpress.XtraRichEdit.API.Native
 Imports System
 
 Namespace LineNumberingExample
-    #Region "#MyLayoutVisitor"
-    Public Class MyLayoutVisitor
-        Inherits LayoutVisitor
+	#Region "#MyLayoutVisitor"
+	Public Class MyLayoutVisitor
+		Inherits LayoutVisitor
 
-        Private document As Document
+		Private document As Document
 
-        Private privateRowIndex As Integer
-        Public Property RowIndex() As Integer
-            Get
-                Return privateRowIndex
-            End Get
-            Private Set(ByVal value As Integer)
-                privateRowIndex = value
-            End Set
-        End Property
+		Private privateRowIndex As Integer
+		Public Property RowIndex() As Integer
+			Get
+				Return privateRowIndex
+			End Get
+			Private Set(ByVal value As Integer)
+				privateRowIndex = value
+			End Set
+		End Property
 
-        Public Sub New(ByVal doc As Document)
-            Me.document = doc
-            RowIndex = 0
-        End Sub
+		Public Sub New(ByVal doc As Document)
+			Me.document = doc
+			RowIndex = 0
+		End Sub
 
-        Protected Overrides Sub VisitRow(ByVal row As LayoutRow)
-            RowIndex += 1
-            MyBase.VisitRow(row)
-        End Sub
-    End Class
-    #End Region ' #MyLayoutVisitor
+		Protected Overrides Sub VisitRow(ByVal row As LayoutRow)
+			RowIndex += 1
+			MyBase.VisitRow(row)
+		End Sub
+	End Class
+	#End Region ' #MyLayoutVisitor
 End Namespace
